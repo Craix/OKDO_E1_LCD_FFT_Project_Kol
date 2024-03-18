6040546d936ded187643318e112a0de1f9094a18
Craix <maciekszostak124@gmail.com>
2024-03-18 16:18:46 +0100

    Simple FFT

--- source/OKDO_E1_LCD_FFT.c
+++ source/OKDO_E1_LCD_FFT.c
@@ -6,8 +6,10 @@
 #include "clock_config.h"
 #include "LPC55S69_cm33_core0.h"
 #include "fsl_debug_console.h"
+
 #include "lcd.h"
 #include "arm_math.h"
+#include "arm_const_structs.h"
 
 #define NFFT 512
 #define N2FFT (2*NFFT)
@@ -18,6 +20,7 @@ float f=0;
 void drawPlot(float *data, uint16_t size, uint16_t color)
 {
 	static float y=0, y1=0;
+
 	y=64-63*(data[0]);
 
 	for(int x=0; x<size-1; x++)
@@ -34,6 +37,23 @@ void drawPlot(float *data, uint16_t size, uint16_t color)
 		LCD_Draw_Line(x, y1, x+1, y, color);
 	}
 }
+
+void drawBars(float *data, uint16_t size, uint16_t color)
+{
+	static float y=0;
+	for(int x=0; x<size; x++)
+	{
+		y=127-127*(data[x]);
+
+		if(y>=LCD_HEIGHT)
+			y=LCD_HEIGHT-1;
+
+		if(y<0)
+			y=0;
+
+		LCD_Draw_Line(x, 127, x, y, color);
+	}
+}
 /*
  * @brief Application entry point.
  */
@@ -43,34 +63,36 @@ int main(void)
 	BOARD_InitBootPins();
 	BOARD_InitBootClocks();
 	BOARD_InitBootPeripherals();
-
 	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
 	/* Init FSL debug console. */
 	BOARD_InitDebugConsole();
 	#endif
 
 	LCD_Init(FLEXCOMM8_PERIPHERAL);
-
 	f=1;
 
 	while(1)
 	{
-
 		for(int i=0;i<NFFT;i++)
 		{
-
 			x[2*i] = arm_sin_f32(2*PI*i*f/NFFT);
 			x[2*i+1] = 0;
 		}
 
-		f+=0.1;
+		f+=0.02;
+
 		if(f>=160)
 			f=1;
 
 		LCD_Clear(0x0000);
 		drawPlot(x, 160, 0x0FF0);
-		LCD_GramRefresh();
-	}
 
-	return 0 ;
+		arm_cfft_f32(&arm_cfft_sR_f32_len512, x, 0, 1);
+		arm_cmplx_mag_f32(x, x, NFFT);
+		arm_scale_f32 (x, 1/256.0, x, NFFT);
+		drawBars(x, 160, 0xF800);
+
+		LCD_GramRefresh();
+}
+return 0 ;
 }
